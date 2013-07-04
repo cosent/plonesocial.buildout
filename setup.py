41eaf38a3f42c2c2be9f12f717182b7e253a619e
Guido A.J. Stevens <guido.stevens@cosent.nl>
2013-07-04 11:17:02 +0000

    add cosent.buildtools

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+version = '0.5.0dev'
+
+setup(name="plonesocial.buildout",
+      version=version,
+      author='Guido Stevens',
+      author_email='guido.stevens@cosent.net')
