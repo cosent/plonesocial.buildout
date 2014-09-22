40769f00dde7e76cdddeba7543ef444351573463
Low Kian Seong <kianseong@gmail.com>
2014-09-22 11:44:38 +0800

    Update setup.py
    Adding more metadata to `setup.py` to be picked up by `pypi` when uploading package.

--- setup.py
+++ setup.py
@@ -5,4 +5,6 @@ version = '0.5.4'
 setup(name="plonesocial.buildout",
       version=version,
       author='Guido Stevens',
-      author_email='guido.stevens@cosent.net')
+      author_email='guido.stevens@cosent.net'
+      description='Development buildout for plonesocial suite.',
+      url='https://github.com/cosent/plonesocial.buildout/', )
